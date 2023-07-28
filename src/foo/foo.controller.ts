2e040a475164c19d0731f5690212eed1b334a92a
LennyTheDude <lennyfromrussia@gmail.com>
2023-07-28 13:01:55 +0530

    Added 4 calculator routes to new controller

--- src/foo/foo.controller.ts
+++ src/foo/foo.controller.ts
@@ -1,18 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { FooService } from './foo.service';
+import { Request } from 'express';
 
 @ApiTags('Foo')
 @Controller({
-  path: 'foo',
+  path: 'foo/calc',
   version: '1',
 })
 export class FooController {
   constructor(private service: FooService) {}
 
-  @Get()
-  foo() {
-    return this.service.foo();
+  @Get('add')
+  add(@Req() req: Request) {
+    return this.service.add({ ...req.query });
+  }
+
+  @Get('subtract')
+  subtract(@Req() req: Request) {
+    return this.service.subtract({ ...req.query });
+  }
+
+  @Get('multiply')
+  multiply(@Req() req: Request) {
+    return this.service.multiply({ ...req.query });
+  }
+
+  @Get('divide')
+  divide(@Req() req: Request) {
+    return this.service.divide({ ...req.query });
   }
 }
--- src/foo/foo.service.ts
+++ src/foo/foo.service.ts
@@ -6,7 +6,27 @@ import { AllConfigType } from 'src/config/config.type';
 export class FooService {
   constructor(private configService: ConfigService<AllConfigType>) {}
 
-  foo() {
-    return { message: 'foo' };
+  add(params: any) {
+    if (!params.num1 || !params.num2)
+      return { error: 'Please provide 2 numbers to add!' };
+    return { result: Number(params.num1) + Number(params.num2) };
+  }
+
+  subtract(params: any) {
+    if (!params.num1 || !params.num2)
+      return { error: 'Please provide 2 numbers to subtract!' };
+    return { result: Number(params.num1) - Number(params.num2) };
+  }
+
+  multiply(params: any) {
+    if (!params.num1 || !params.num2)
+      return { error: 'Please provide 2 numbers to multiply!' };
+    return { result: Number(params.num1) * Number(params.num2) };
+  }
+
+  divide(params: any) {
+    if (!params.num1 || !params.num2)
+      return { error: 'Please provide 2 numbers to divide!' };
+    return { result: Number(params.num1) / Number(params.num2) };
   }
 }
